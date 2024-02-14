2b8f96fbcc3767d8755d5d83299efcc72d015c07
joonanykanen <joona.nykanen@icloud.com>
2024-02-14 17:52:57 +0200

    dep: Fixed dependencies

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,7 +1,7 @@
 // routes/api.js, JN, 14.02.2024
 const express = require('express');
 const router = express.Router();
-const userRouter = require('.api/user');
+const userRouter = require('./api/user');
 
 router.use('/user', userRouter);
 
