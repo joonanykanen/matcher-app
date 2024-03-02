4943b5fb7556644f6bba48b25df7ec480f722ce1
joonanykanen <joona.nykanen@icloud.com>
2024-03-02 17:49:53 +0200

    style: Improvements to the SwipeCard

--- client/src/components/Swipe/SwipeCard.js
+++ client/src/components/Swipe/SwipeCard.js
@@ -1,17 +1,36 @@
 // src/components/Swipe/SwipeCard.js, JN, 21.02.2024
 import React from 'react';
-import { Card, CardContent, Typography, Button } from '@mui/material';
-import ProfilePic from '../Profile/ProfilePic';
+import { Card, CardContent, Typography, Button, Avatar, Box } from '@mui/material';
 
 const SwipeCard = ({ profile, handleLike, handleDislike }) => {
   return (
-    <Card>
+    <Card sx={{
+      maxWidth: 345,
+      margin: 'auto',
+      position: 'relative',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      padding: 2,
+    }}>
       <CardContent>
-        <ProfilePic imageUrl={profile.profilePic} />
-        <Typography variant="h5">{profile.firstName} {profile.lastName}, {profile.age}</Typography>
-        <Typography variant="body1">{profile.bio ?? 'No bio available'}</Typography>
-        <Button variant="contained" color="primary" onClick={() => handleLike(profile)}>Like</Button>
-        <Button variant="contained" color="secondary" onClick={() => handleDislike(profile)}>Dislike</Button>
+        <Avatar src={`/${profile.profilePic}`} alt="Profile" sx={{ 
+          width: 160, // Adjust these values as needed
+          height: 160,
+          mb: 2,
+        }} />
+        <Typography gutterBottom variant="h5" align="center">{profile.firstName} {profile.lastName}, {profile.age}</Typography>
+        <Typography variant="body2" color="textSecondary" align="center">{profile.bio ?? 'No bio available'}</Typography>
+        <Box sx={{ 
+          display: 'flex',
+          justifyContent: 'space-around',
+          width: '100%',
+          mt: 2,
+        }}>
+          <Button variant="contained" color="primary" onClick={() => handleLike(profile)}>Like</Button>
+          <Button variant="contained" color="secondary" onClick={() => handleDislike(profile)}>Dislike</Button>
+        </Box>
       </CardContent>
     </Card>
   );
